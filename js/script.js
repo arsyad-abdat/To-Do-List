c9361b59b9d7ce92336efd45fef4aed98d052607
arsyad-abdat <102569942+arsyad-abdat@users.noreply.github.com>
2022-03-29 13:18:00 +0800

    Added text to todo list

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
 // Step 1: Find Element
 let addTaskButton = document.getElementById("add-task")
 let newTaskInput = document.getElementById("task-input")
+let todoListContainer = document.getElementById("todo-list") //go search webpage for thing with to do list tag
 
 // Step 2: Write a function to implement the behavioru
 
 function onAddTaskClicked(event) {
     let taskName = newTaskInput.ariaValueMax;
-    newTaskInput.value = "";
+    newTaskInput.value = ""; //Wipe content
 
     //log it to console
     console.log(taskName)
+
+    // When that event happens, read input, take input and put it into container. take the todo list container
+    // then insert adjacent html
+    todoListContainer.insertAdjacentHTML("afterbegin",taskName);
+
 }
 
 // Step 3: Link element, function and event
