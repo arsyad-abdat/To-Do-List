cb274440204e5f554ef066dee3578154798080d2
arsyad-abdat <102569942+arsyad-abdat@users.noreply.github.com>
2022-03-29 12:45:58 +0800

    Added add task behaviour

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-// This is empty
+// Step 1: Find Element
+let addTaskButton = document.getElementById("add-task")
+let newTaskInput = document.getElementById("task-input")
+
+// Step 2: Write a function to implement the behavioru
+
+function onAddTaskClicked(event) {
+    let taskName = newTaskInput.ariaValueMax;
+    newTaskInput.value = "";
+
+    //log it to console
+    console.log(taskName)
+}
+
+// Step 3: Link element, function and event
+// Add event listener, for when they click 
+addTaskButton.addEventListener('click',onAddTaskClicked)
